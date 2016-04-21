68ecfcd5d67231a4ff0751bf4f374e81681dca5f
diegob94 <dbim.bena@gmail.com>
2016-04-21 19:22:15 -0300

    cambio prueba buzzer

--- Software/Codigos de prueba/test_unplug_buzzer/test_unplug_buzzer.ino
+++ Software/Codigos de prueba/test_unplug_buzzer/test_unplug_buzzer.ino
@@ -1,15 +1,16 @@
+#define BUZZER A0
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A0, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 1500; i > 10; i = int(i / 1.2)) {
-    tone(A0, 4000);
+    tone(BUZZER, 4000);
     if (i > 13) {
       delay(30);
-      noTone(A0);
+      noTone(BUZZER);
     }
     else
       delay(1500);
@@ -18,6 +19,6 @@ void loop() {
     else
       delay(i);
   }
-  noTone(A0);
+  noTone(BUZZER);
   delay(10000);
 }
